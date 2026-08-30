754d3d2eed1e9a4df6d979f612770f054ef5f2ce
Johan Salazar Atencio <johan.salazar@tecsup.edu.pe>
2026-08-30 00:20:21 -0500

    Asignacion del porcentaje de interes por cuotas

--- Sem03/Lab03/app/src/main/java/com/salazar/lab03/Main.kt
+++ Sem03/Lab03/app/src/main/java/com/salazar/lab03/Main.kt
@@ -17,4 +17,18 @@ fun main() {
 
     print("Ingrese Cantidad            : ")
     val cantidad = scanner.nextInt()
+
+    print("Ingrese el N° Cuotas (6, 12 o 24): ")
+    val cuotas = scanner.nextInt()
+
+    //Agregamos el porcentaje del interes
+    val pctInteres = when (cuotas) {
+        6->0.20
+        12->0.40
+        24->0.60
+        else -> {
+            println("\n[ERROR] estas cuotas no son validas. Solo admitidmos 6, 12 o 24.")
+            return
+        }
+    }
 }
