8b744414fff884135aaf9e849952e85005662d33
Johan Salazar Atencio <johan.salazar@tecsup.edu.pe>
2026-09-07 23:31:11 -0500

    configurar estructura base y fondo degradado

--- Sem03/Ejercicio Practico/app/src/main/java/com/salazar/registrodenotas/MainActivity.kt
+++ Sem03/Ejercicio Practico/app/src/main/java/com/salazar/registrodenotas/MainActivity.kt
@@ -4,12 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.salazar.registrodenotas.ui.theme.RegistroDeNotasTheme
 
@@ -20,10 +27,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             RegistroDeNotasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    RegistroNotasScreen(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +35,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun RegistroNotasScreen(modifier: Modifier = Modifier) {
+    Box(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .background(
+                Brush.verticalGradient(
+                    colors = listOf(Color(0xFFEDE7F6), Color(0xFFF3E5F5))
+                )
+            )
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
+        ) {}
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun RegistroNotasPreview() {
     RegistroDeNotasTheme {
-        Greeting("Android")
+        RegistroNotasScreen()
     }
 }
