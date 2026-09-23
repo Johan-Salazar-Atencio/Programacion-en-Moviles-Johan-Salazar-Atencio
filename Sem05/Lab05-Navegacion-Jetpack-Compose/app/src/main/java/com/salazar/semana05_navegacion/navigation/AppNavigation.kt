7c244475a10dce2ee0920030289dbf858bedc190
Johan Salazar Atencio <johan.salazar@tecsup.edu.pe>
2026-09-22 21:56:31 -0500

    Agrega estructura de navegación en AppNavigation.kt

--- Sem05/Lab05-Navegacion-Jetpack-Compose/app/src/main/java/com/salazar/semana05_navegacion/navigation/AppNavigation.kt
+++ Sem05/Lab05-Navegacion-Jetpack-Compose/app/src/main/java/com/salazar/semana05_navegacion/navigation/AppNavigation.kt
@@ -1,2 +1,55 @@
 package com.salazar.semana05_navegacion.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHost
+import androidx.navigation.NavType
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.navArgument
+
+@Composable
+fun AppNavigation() {
+    //para recordar el controlador de navegacion
+    val navController = rememberNavController()
+
+    //Definir el NavHost con la ruta de inioio
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ){
+        //Ruta1: Inicio
+        composable(route = Screen.Home.route) {
+            HomeScreen(navController = navController)
+        }
+
+        //Ruta2 Lista de elementos
+        composable(route = Screen.List.route) {
+            ListScreen(navController = navController)
+        }
+
+        //Ruta3 Perfil
+        composable(route = Screen.Profile.route) {
+            ProfileScreen(navController = navController)
+        }
+
+        //Ruta4 Detalle con parametro "itemId"
+        composable(
+            route = Screen.Detail.route,
+            arguments = listOf(
+                navArgument(name = "itemId") {
+                    type = NavType.IntType
+                    defaultValue = 0
+                }
+            )
+        ) {
+            backStackEntry ->
+            //Extraeremos el numero "itemId" de la ruta
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+
+            //Le pasamos el itemId recibido a la pantalla de detalle
+            DetailScreen(navController = navController, itemId = itemId)
+
+        }
+    }
+}
