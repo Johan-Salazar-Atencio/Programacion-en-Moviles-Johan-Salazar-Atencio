42b7c4a666496cc78bc813446ffec2da5bb5a2d3
Johan Salazar Atencio <johan.salazar@tecsup.edu.pe>
2026-09-23 03:17:01 -0500

    define rutas de navegacion en Screen.kt

---
+++ Sem05/Tecsup fit/app/src/main/java/com/salazar/tecsupfit/navigation/Screen.kt
@@ -0,0 +1,12 @@
+package com.salazar.tecsupfit.navigation
+sealed class Screen(val route: String) {
+    object Home : Screen("home")
+    object Detail : Screen("detail/{classId}") {
+        fun createRoute(classId: Int) = "detail/$classId"
+    }
+    object Confirmation : Screen("confirmation/{className}/{classTime}/{classRoom}") {
+        fun createRoute(name: String, time: String, room: String) = "confirmation/$name/$time/$room"
+    }
+    object Reservas : Screen("reservas")
+    object Profile : Screen("profile")
+}
